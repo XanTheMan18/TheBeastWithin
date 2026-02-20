699b9025444b01590ef400026b2420c26344292b
XanTheMan18 <berenale000@alpinesd.org>
2026-02-20 10:10:37 -0700

    Added incomplete script and complete anim

--- Assets/Scripts/Weapons/HandgunFire.cs
+++ Assets/Scripts/Weapons/HandgunFire.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class HandgunFire : MonoBehaviour
 {
     [SerializeField] AudioSource Gunfire;
-    
+    [SerializeField] GameObject handgun;
     void Update()
     {
         if (Input.GetMouseButton(0)) 
         {
             Gunfire.Play();
         }
+    }
+
+    IEnumerator FiringGun()
+    {
+        Gunfire.Play();
+        handgun.GetComponant<Animator>().Play("HandgunFire");
     }
 }
