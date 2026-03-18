666414a7531a1383e66a6fac6b3520b68063d9f1
David-H-124 <hokedav001@alpinesd.org>
2026-03-18 10:05:27 -0600

    did some animation stuff but it no work

--- Assets/Scripts/AutoDoorTrigger.cs
+++ Assets/Scripts/AutoDoorTrigger.cs
@@ -1,6 +1,59 @@
 using UnityEngine;
 
-public class AutoDoorTrigger
+public class AutoDoorTrigger : MonoBehaviour
 {
-    
+    [SerializeField] private Animator doorAnimator;
+    [SerializeField] private string openTriggerName = "Open";
+    [SerializeField] private string closeTriggerName = "Close";
+    [SerializeField] private float closeDelay = 2f;
+
+    private bool isDoorOpen = false;
+    private Coroutine closeCoroutine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ScheduleCloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        if (!isDoorOpen)
+        {
+            isDoorOpen = true;
+            doorAnimator.SetTrigger(openTriggerName);
+
+            if (closeCoroutine != null)
+                StopCoroutine(closeCoroutine);
+        }
+    }
+
+    private void ScheduleCloseDoor()
+    {
+        if (closeCoroutine != null)
+            StopCoroutine(closeCoroutine);
+
+        closeCoroutine = StartCoroutine(CloseDoorAfterDelay());
+    }
+
+    private System.Collections.IEnumerator CloseDoorAfterDelay()
+    {
+        yield return new WaitForSeconds(closeDelay);
+        
+        if (isDoorOpen)
+        {
+            isDoorOpen = false;
+            doorAnimator.SetTrigger(closeTriggerName);
+        }
+    }
 }
